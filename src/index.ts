1da16465bccc927803ba141de6729b4d268a4cd3
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-18 19:25:21 +1200

    feat: I FUCKING HATE CORS WHAT THE FUCK IS CORS, NAH WHAT THE FUCK FUCK CORS 😡😤🖕

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
+import "dotenv/config";
+
 import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.all("/", async (req, res) => {
   return res.sendStatus(200);
 });
 
 app.listen(3001, () => {
-  console.log("API listening on port 3000");
+  console.log("API listening on port 3001");
 });
