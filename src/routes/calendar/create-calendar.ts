6e209d3eb51cbedc9b27a10938dd71b91bfa53f5
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-19 21:22:58 +1200

    feat: endpoint to create a calendar

---
+++ src/routes/calendar/create-calendar.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import { z } from "zod";
+import { validateRequest } from "zod-express-middleware";
+
+import { authorizeRequest } from "../../core/middleware/authorize";
+import { db } from "../../core/drizzle/db";
+import { timetable } from "../../core/drizzle/schema";
+
+export const createCalendar = express.Router();
+
+createCalendar.post(
+  "/",
+  authorizeRequest,
+  validateRequest({
+    body: z.object({ tableName: z.string() }),
+  }),
+  async (req, res) => {
+    if (!req.id) {
+      return res.sendStatus(400);
+    }
+
+    // Now we just need to create the table
+    try {
+      await db.insert(timetable).values({
+        name: req.body.tableName,
+        userId: req.id,
+      });
+    } catch (err) {
+      return res.sendStatus(500);
+    }
+
+    return res.sendStatus(200);
+  }
+);
