d5b9be449cec1e681dc7c8244dfa57d17ba3f23a
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-19 21:23:25 +1200

    feat: endpoint to fetch all the calendars for a user

---
+++ src/routes/calendar/fetch-all-calendars.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import { z } from "zod";
+import { validateRequest } from "zod-express-middleware";
+
+import { authorizeRequest } from "../../core/middleware/authorize";
+import { db } from "../../core/drizzle/db";
+import { timetable } from "../../core/drizzle/schema";
+import { eq } from "drizzle-orm";
+
+export const fetchCalendarsSelf = express.Router();
+
+fetchCalendarsSelf.get(
+  "/:userId",
+  authorizeRequest,
+  validateRequest({
+    params: z.object({
+      userId: z.string(),
+    }),
+  }),
+  async (req, res) => {
+    if (!req.id) {
+      return res.sendStatus(400);
+    }
+
+    let drizzleRes;
+
+    // Fetch all the timetables with the user if provided as a param
+    try {
+      drizzleRes = await db
+        .select()
+        .from(timetable)
+        .where(eq(timetable.userId, req.params.userId));
+    } catch (err) {
+      return res.sendStatus(500);
+    }
+
+    return res.send(drizzleRes);
+  }
+);
