fd53496c11e98640fa0190e8b94e4fec75a5afe8
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-20 09:43:31 +1200

    feat: endpoint for fetching a specific calendar is added

---
+++ src/routes/calendar/fetch-specific-calendar.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import { z } from "zod";
+import { validateRequest } from "zod-express-middleware";
+
+import { authorizeRequest } from "../../core/middleware/authorize";
+import { db } from "../../core/drizzle/db";
+import { timetable } from "../../core/drizzle/schema";
+import { eq, and } from "drizzle-orm";
+
+export const fetchSpecificCalendarSelf = express.Router();
+
+fetchSpecificCalendarSelf.get(
+  "/:userId/:calendarId",
+  authorizeRequest,
+  validateRequest({
+    params: z.object({
+      userId: z.string(),
+      calendarId: z.string(),
+    }),
+  }),
+  async (req, res) => {
+    if (!req.id) {
+      return res.sendStatus(400);
+    }
+
+    let drizzleRes;
+
+    // Fetch all the timetables with the user if provided as a param
+    try {
+      drizzleRes = await db
+        .select()
+        .from(timetable)
+        .where(
+          and(
+            eq(timetable.userId, req.params.userId),
+            eq(timetable.id, req.params.calendarId)
+          )
+        );
+    } catch (err) {
+      return res.sendStatus(500);
+    }
+
+    return res.send(drizzleRes);
+  }
+);
