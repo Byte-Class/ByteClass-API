e8f32573d4b757a74fd5169988f3c78b23e00a72
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-20 09:44:36 +1200

    feat: added the routes to the index file

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,12 @@ app.use(cors());
 // Import all routes
 import { createCalendar } from "./routes/calendar/create-calendar";
 import { fetchCalendarsSelf } from "./routes/calendar/fetch-all-calendars";
+import { fetchSpecificCalendarSelf } from "./routes/calendar/fetch-specific-calendar";
 
 // Use all the routes
 app.use("/api/calendars", createCalendar);
 app.use("/api/calendars", fetchCalendarsSelf);
+app.use("/api/calendars", fetchSpecificCalendarSelf);
 
 app.all("/", async (req, res) => {
   return res.sendStatus(200);
