3b02daf6e7ada852d4d7025ac4a5dc715a89328e
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-28 16:26:59 +1200

    feat: eslint added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,16 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      // Note: you must disable the base rule as it can report incorrect errors
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warning",
+    },
+  },
 ];
